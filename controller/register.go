0d3c7a5399d6461fead3d120c562cc26932191e8
sanser <sansermail@163.com>
2022-01-21 19:50:31 +0800

    redis验证

--- controller/register.go
+++ controller/register.go
@@ -1,34 +1,34 @@
 package controller
 
 import (
-	"Goto/models"
-	"Goto/service"
 	"github.com/gin-gonic/gin"
+	"jingdong/models"
+	"jingdong/service"
 )
 
-
-func Register(c *gin.Context)  {
+func Register(c *gin.Context) {
 	//接受参数
 	var register models.Register
-	err :=c.ShouldBind(&register)
-	if err!=nil {
-		c.JSON(200,gin.H{
-			"state":false,
-			"msg":"参数绑定失败",
-		})
-	}
-	//参数传递 进行校验
-	ok,code :=service.Register(register)
-	if!ok{
-		c.JSON(200,gin.H{
-			"state":"false",
-			"msg":code,
+	err := c.ShouldBind(&register)
+	if err != nil {
+		c.JSON(200, gin.H{
+			"state": false,
+			"msg":   "参数绑定失败",
 		})
 		return
 	}
-	c.JSON(200,gin.H{
-		"state":"ture",
-		"msg":code,
+	//参数传递 进行校验
+	ok, code := service.Register(register)
+	if !ok {
+		c.JSON(200, gin.H{
+			"state": "false",
+			"msg":   code,
+		})
+		return
+	}
+	c.JSON(200, gin.H{
+		"state": "ture",
+		"msg":   code,
 	})
 	return
 
