a6062f054430aaabae1df7cd271766cb53d74821
sanser <sansermail@163.com>
2022-02-18 13:26:53 +0800

    更新调用方式 减少返回参数

--- controller/register.go
+++ controller/register.go
@@ -1,48 +1,17 @@
 package controller
 
 import (
+	"JD/dao"
 	"JD/models"
-	"JD/service"
 	"JD/utils"
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
-func Register(c *gin.Context) {
-	//接受参数
-	var register models.Register
-
-	err := c.ShouldBind(&register)
-	fmt.Println(err)
-	fmt.Println(register)
-	if err != nil {
-
-		c.JSON(200, gin.H{
-			"state": false,
-			"msg":   "参数绑定失败",
-		})
-		return
-	}
-	//参数传递 进行校验
-	ok, code := service.Register(register)
-	if !ok {
-		c.JSON(200, gin.H{
-			"state": false,
-			"msg":   code.Error(),
-		})
-		return
-	}
-	c.JSON(200, gin.H{
-		"state": true,
-		"msg":   "注册成功",
-	})
-	return
-
-}
-
 func SendMessage(c *gin.Context) {
 	number := c.Query("Phone")
 	fmt.Println(number)
+	//调用工具发送短信
 	err := utils.Sendsms(number)
 	if err != nil {
 		c.JSON(200, gin.H{
@@ -55,4 +24,42 @@ func SendMessage(c *gin.Context) {
 		"state": true,
 		"msg":   "短信发送成功",
 	})
+}
+
+func Register(c *gin.Context) {
+	//接受参数
+	var register models.Register
+
+	err := c.ShouldBind(&register)
+	if err != nil {
+
+		c.JSON(200, gin.H{
+			"state": false,
+			"msg":   "参数绑定失败",
+		})
+		return
+	}
+	//参数传递 进行校验
+	err = utils.GetCk(register.Number, register.Code)
+	if err != nil {
+		c.JSON(200, gin.H{
+			"state": false,
+			"msg":   err.Error(),
+		})
+		return
+	}
+	err = dao.Register(register.Username, register.Password, register.Number)
+	if err != nil {
+		c.JSON(200, gin.H{
+			"state": false,
+			"msg":   err.Error(),
+		})
+		return
+	}
+	c.JSON(200, gin.H{
+		"state": true,
+		"msg":   "注册成功",
+	})
+	return
+
 }
